38eb4c77410d1c13fb3933c5d914d025769e3a44
Stefan <sb121@rice.edu>
2025-10-04 18:25:11 -0500

    done with mergeintofile

--- Main/DatabaseTable/source/Sorting.cc
+++ Main/DatabaseTable/source/Sorting.cc
@@ -10,7 +10,31 @@
 
 using namespace std;
 
-void mergeIntoFile (MyDB_TableReaderWriter &, vector <MyDB_RecordIteratorAltPtr> &, function <bool ()>, MyDB_RecordPtr, MyDB_RecordPtr) {}
+void mergeIntoFile (MyDB_TableReaderWriter &sortIntoMe, vector <MyDB_RecordIteratorAltPtr> &mergeUs, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
+    auto cmp = [&lhs, &rhs, &comparator](MyDB_RecordPtr a, MyDB_RecordPtr b) { 
+        lhs = a;
+        rhs = b;
+        return comparator();
+    };
+    map<MyDB_RecordPtr, int, decltype(cmp)> heap(cmp);
+    for (int i = 0; i < mergeUs.size(); i++) {
+        MyDB_RecordPtr temp = make_shared<MyDB_Record>(lhs->getSchema());
+        mergeUs[i]->getCurrent(temp);
+        heap[temp] = i;
+    }
+    while (!heap.empty()) {
+        auto pop = heap.begin();
+        MyDB_RecordPtr record = pop->first;
+        int idx = pop->second;
+        heap.erase(pop);
+        sortIntoMe.append(record);
+        if (mergeUs[idx]->advance()) {
+            MyDB_RecordPtr temp = make_shared<MyDB_Record>(lhs->getSchema());
+            mergeUs[idx]->getCurrent(temp);
+            heap[temp] = idx;
+        }
+    }
+}
 
 vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr, MyDB_RecordIteratorAltPtr, MyDB_RecordIteratorAltPtr, function <bool ()>, 
 	MyDB_RecordPtr, MyDB_RecordPtr) {return vector <MyDB_PageReaderWriter> (); } 

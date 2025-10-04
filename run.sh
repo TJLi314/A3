d7776ffc0967866ea7a8f394435dd7803ec295a1
TJ Li <TianjianLi314@gmail.com>
2025-10-04 11:29:55 -0500

    Edit SConstruct and include header to allow to build

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 clear
 cd /storage-home/t/tl107/comp432/A3/Build
-echo 4 | ~/scons/bin/scons-3.1.2
+echo 6 | ~/scons/bin/scons-3.1.2
 if [ -n "$1" ]; then
-    bin/recordUnitTest > "../$1"
+    bin/sortUnitTest > "../$1"
 else
-    bin/recordUnitTest
+    bin/sortUnitTest
 fi

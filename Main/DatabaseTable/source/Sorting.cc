d9a7bfb6649f63ca64ba91dd304e802ad545cb91
TJ Li <TianjianLi314@gmail.com>
2025-10-04 19:16:43 -0500

    Sorting for sort list

--- Main/DatabaseTable/source/Sorting.cc
+++ Main/DatabaseTable/source/Sorting.cc
@@ -36,9 +36,54 @@ void mergeIntoFile (MyDB_TableReaderWriter &sortIntoMe, vector <MyDB_RecordItera
     }
 }
 
-vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr, MyDB_RecordIteratorAltPtr, MyDB_RecordIteratorAltPtr, function <bool ()>, 
-	MyDB_RecordPtr, MyDB_RecordPtr) {return vector <MyDB_PageReaderWriter> (); } 
+vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr parent, MyDB_RecordIteratorAltPtr leftIter, 
+	MyDB_RecordIteratorAltPtr rightIter, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
+	// Get anonymous storage from buffer manager
+ 
+    // Iterate through both iterators, do a comparison, and add to anonymous storag
+	return vector <MyDB_PageReaderWriter> (); 
+} 
 	
-void sort (int, MyDB_TableReaderWriter &, MyDB_TableReaderWriter &, function <bool ()>, MyDB_RecordPtr, MyDB_RecordPtr) {} 
+void sort (int runSize, MyDB_TableReaderWriter &sortMe, MyDB_TableReaderWriter &sortIntoMe, 
+	function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
+
+	vector<MyDB_RecordIteratorAltPtr> sortedRunPtrs;
+    int i = 0;
+    while (i < sortMe.getNumPages() - 1) {
+        // Load a run of pages into RAM .
+        // Each page will correspond to a MyDB_PageReaderWriter object, and
+        // all of those objects will be stored in a std :: vector.
+        vector<vector<MyDB_PageReaderWriter>> run;
+        for (; i < i + runSize && i < sortMe.getNumPages(); i++) {
+            MyDB_PageReaderWriter page = sortMe[i];
+ 
+            // Sort each individual page in the vector
+            page.sortInPlace(comparator, lhs, rhs);
+            vector<MyDB_PageReaderWriter> pageList = { page };
+            run.push_back(pageList);
+        }
+ 
+        // go through the list, for each adjacent pair of pages in the list, use the mergeIntoList function
+        while (run.size() > 1) {
+            vector<vector<MyDB_PageReaderWriter>> newVector;
+ 
+            for (int j = 0; j < run.size(); j += 2) {
+                if (j == run.size() - 1) {
+                    newVector.push_back(run[j]);
+                    continue;
+                }
+                MyDB_RecordIteratorAltPtr leftIter = getIteratorAlt(run[j]);
+                MyDB_RecordIteratorAltPtr rightIter = getIteratorAlt(run[j + 1]);
+                vector <MyDB_PageReaderWriter> sortedPages = mergeIntoList(sortMe.getBufferMgr(), leftIter, rightIter, comparator, lhs, rhs);
+            }
+ 
+            run = newVector;
+        }
+ 
+        // there should only be one vector of pageReaderWriters now in the run
+        MyDB_RecordIteratorAltPtr sortedRunPtr = getIteratorAlt(run[0]);
+        sortedRunPtrs.push_back(sortedRunPtr);
+    }
+} 
 
 #endif
